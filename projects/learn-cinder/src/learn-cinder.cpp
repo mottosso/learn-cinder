6c149b57e8f7851c566e15244047c9b756f2f217
Marcus Ottosson <konstruktion@gmail.com>
2016-03-19 19:28:32 +0000

    Complete part 3

--- projects/learn-cinder/src/learn-cinder.cpp
+++ projects/learn-cinder/src/learn-cinder.cpp
@@ -2,14 +2,41 @@
 #include "cinder/app/RendererGl.h"
 #include "cinder/gl/gl.h"
 #include "cinder/gl/Shader.h"
+#include "cinder/Easing.h"
 
 using namespace ci;
 using namespace ci::app;
 
 class BasicApp : public App {
 public:
+	void setup() override;
     void draw() override;
+
+	static const int NUM_SLICES = 4;
+
+	CameraPersp mCam;
+	gl::BatchRef mSlices[NUM_SLICES];
 };
+
+void BasicApp::setup()
+{
+	auto lambert = gl::ShaderDef().lambert().color();
+	gl::GlslProgRef shader = gl::getStockShader(lambert);
+	
+	for (int i = 0; i < NUM_SLICES; ++i)
+	{
+		float rel = i / (float)NUM_SLICES;
+		float sliceHeight = 1.0f / NUM_SLICES;
+		auto slice = geom::Cube().size(1, sliceHeight, 1);
+		auto trans = geom::Translate(0, rel, 0);
+		auto color = geom::Constant(
+			geom::COLOR,
+			Color(CM_HSV, rel, 1, 1));
+		mSlices[i] = gl::Batch::create(slice >> trans >> color, shader);
+	}
+
+	mCam.lookAt(vec3(2, 3, 2), vec3(0, 0.5f, 0));
+}
 
 void BasicApp::draw()
 {
@@ -18,34 +45,34 @@ void BasicApp::draw()
 	gl::enableDepthRead();
 	gl::enableDepthWrite();
 
-	CameraPersp cam;
-	cam.lookAt(vec3(3, 4.5, 4.5), vec3(0, 1, 0));
-	gl::setMatrices(cam);
+	gl::setMatrices(mCam);
 
-	auto lambert = gl::ShaderDef().lambert().color();
-	auto shader = gl::getStockShader(lambert);
-	shader->bind();
+	const float delay = 0.25f;
+	const float rotationTime = 1.5f;
+	const float rotationOffset = 0.1f;
+	const float totalTime = delay + rotationTime + NUM_SLICES * rotationOffset;
 
-	int numSpheres = 64;
-	float maxAngle = M_PI * 7;
-	float spiralRadius = 1;
-	float height = 3;
-	float anim = getElapsedFrames() / 30.0f;
+	// loop every `totalTime` seconds
+	float time = fmod(getElapsedFrames() / 30.0f, totalTime);
 
-	for (int s = 0; s < numSpheres; ++s)
+	for (int i = 0; i < NUM_SLICES; ++i)
 	{
-		float rel = s / (float)numSpheres;
-		float angle = rel * maxAngle;
-		float y = fabs(cos(rel * M_PI + anim));
-		float r = rel * spiralRadius;
-		vec3 offset(r * cos(angle), y, r * sin(angle));
+		// animates from 0 to 1
+		float rotation = 0;
 
-		gl::pushModelMatrix();
-		gl::translate(offset);
-		gl::scale(vec3(0.05f, y, 0.05f));
-		gl::color(Color(CM_HSV, rel, 1, 1));
-		gl::drawCube(vec3(), vec3(1));
-		gl::popModelMatrix();
+		// when does the slice begin rotating
+		float startTime = i * rotationOffset;
+		float endTime = startTime + rotationTime;
+		if (time > startTime && time < endTime)
+		{
+			rotation = (time - startTime) / rotationTime;
+		}
+
+		float angle = easeInOutQuint(rotation) * M_PI / 2.0f;
+
+		gl::ScopedModelMatrix scpModelMtx;
+		gl::rotate(angleAxis(angle, vec3(0, 1, 0)));
+		mSlices[i]->draw();
 	}
 }
 
