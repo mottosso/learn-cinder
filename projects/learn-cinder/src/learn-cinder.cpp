127d1c413900ac202d71d027ed86f2057a478c07
Marcus Ottosson <konstruktion@gmail.com>
2016-03-20 12:01:01 +0000

    Completed part 5

--- projects/learn-cinder/src/learn-cinder.cpp
+++ projects/learn-cinder/src/learn-cinder.cpp
@@ -13,23 +13,62 @@ public:
     void draw() override;
 
 	CameraPersp mCam;
-	gl::BatchRef mSphere;
-	gl::TextureRef mTexture;
+	gl::BatchRef mRect;
 	gl::GlslProgRef mGlsl;
+	gl::Texture2dRef mTex;
 };
+
 
 void BasicApp::setup()
 {
 	mCam.lookAt(vec3(3, 2, 4), vec3(0));
 
-	auto img = loadImage(loadAsset("checkerboard.png"));
-	mTexture = gl::Texture::create(img);
-	mTexture->bind();
+	//auto img = loadImage(loadAsset("texture.jpg"));
 
-	auto shader = gl::ShaderDef().texture().lambert();
-	mGlsl = gl::getStockShader(shader);
-	auto sphere = geom::Sphere().subdivisions(50);
-	mSphere = gl::Batch::create(sphere, mGlsl);
+	mGlsl = gl::GlslProg::create(gl::GlslProg::Format()
+	.vertex(CI_GLSL(150,
+		uniform mat4 ciModelViewProjection;
+		in vec4 ciPosition;
+		in vec2 ciTexCoord0;
+		out vec2 TexCoord0;
+
+		float offset(vec2 uv)
+		{
+			return (sin(uv.x * 15.0) +
+					cos(uv.y * 7.0f + uv.x * 13.0f)) * 0.1f;
+		}
+
+		void main(void)
+		{
+			vec4 pos = ciPosition;
+			pos.y = offset(ciTexCoord0);
+			gl_Position = ciModelViewProjection * pos;
+			TexCoord0 = ciTexCoord0;
+		}
+	))
+	.fragment(CI_GLSL(150,
+		uniform float uCheckSize;
+
+		in vec2 TexCoord0;
+		out vec4 oColor;
+
+		vec4 checker(vec2 uv)
+		{
+			float v = floor(uCheckSize * uv.x) + floor(uCheckSize * uv.y);
+			if (mod(v, 2.0) < 1.0)
+				return vec4(1, 1, 1, 1);
+			else
+				return vec4(0, 0, 0, 1);
+		}
+
+		void main(void)
+		{
+			oColor = checker(TexCoord0);
+		}
+	)));
+
+	auto plane = geom::Plane().subdivisions(ivec2(30));
+	mRect = gl::Batch::create(plane, mGlsl);
 
 	gl::enableDepthWrite();
 	gl::enableDepthRead();
@@ -40,13 +79,10 @@ void BasicApp::draw()
     gl::clear(Color(0.2f, 0.2f, 0.2f));
 	gl::setMatrices(mCam);
 
-	mSphere->draw();
-
-	gl::setMatricesWindow(getWindowSize());
-	Rectf drawRect(0, 0,
-		mTexture->getWidth() / 3,
-		mTexture->getHeight() / 3);
-	gl::draw(mTexture, drawRect);
+	mGlsl->uniform("uCheckSize", 30.0f);
+	float anim = -1.0f * getElapsedFrames() / 30.0f;
+	gl::rotate(angleAxis(anim, vec3(0, 1, 0)));
+	mRect->draw();
 }
 
 CINDER_APP( BasicApp, RendererGl )
